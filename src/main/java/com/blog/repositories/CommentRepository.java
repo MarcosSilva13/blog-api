ea93d3a3029403fb75dacc52554347df3d4e864d
MarcosSilva13 <marcosslp45@gmail.com>
2024-09-30 14:08:03 -0300

    Refactor: add jpa specification executor

--- src/main/java/com/blog/repositories/CommentRepository.java
+++ src/main/java/com/blog/repositories/CommentRepository.java
@@ -2,19 +2,13 @@ package com.blog.repositories;
 
 import com.blog.entities.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.util.List;
 import java.util.Optional;
 
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-
-    @Query(value = """
-            SELECT comment FROM Comment comment JOIN FETCH comment.user user
-            WHERE comment.post.postId = :postId
-            """)
-    List<Comment> findAllByPostId(@Param("postId") Long postId);
+public interface CommentRepository extends JpaRepository<Comment, Long>, JpaSpecificationExecutor<Comment> {
 
     @Query(value = """
             SELECT comment FROM Comment comment JOIN FETCH comment.user user
--- src/main/java/com/blog/repositories/PostRepository.java
+++ src/main/java/com/blog/repositories/PostRepository.java
@@ -4,12 +4,9 @@ import com.blog.entities.Post;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 
-public interface PostRepository extends JpaRepository<Post, Long> {
+public interface PostRepository extends JpaRepository<Post, Long>, JpaSpecificationExecutor<Post> {
 
-    @Query(value = """
-            SELECT post FROM Post post JOIN FETCH post.user user
-            """)
-    Page<Post> findAll(Pageable pageable);
 }
